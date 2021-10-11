562ac7a4e7d89b230b806d3facc11e2856b22e35
arpeiks <arpeiks@gmail.com>
2021-10-11 13:42:25 +0100

    Listing 2.6

---
+++ 2.6.java
@@ -0,0 +1,6 @@
+// Listing 2.6 Using the Person class [Listing 2.5] from Java
+Person person = new Person("Bob", true);
+
+System.out.println(person.getName()); // Bob
+
+System.out.println(person.isMarried()) // true
