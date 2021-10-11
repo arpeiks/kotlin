e800e2a640c4181ec634a7cf6ca71074f41e1db4
arpeiks <arpeiks@gmail.com>
2021-10-11 11:57:29 +0100

    Listing 1.1

--- 1.1.kt
+++ 1.1.kt
@@ -2,8 +2,11 @@ data class Person(val name: String, val age: Int? = null)
 
 fun main() {
 
-    val persons = listOf(Person("Alice"), Person("Bob", age = 29))
 
+    val Alice = Person(name = "Alice")
+    val Bob = Person(name = "Bob", age = 29)
+
+    val persons = listOf(Alice, Bob)
     val oldest = persons.maxByOrNull { it.age ?: 0 }
 
     println("The oldest is: $oldest")
