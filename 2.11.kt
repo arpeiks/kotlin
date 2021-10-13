39f8c609ff96d9bb1aacefab58902ae71cc4e727
arpeiks <arpeiks@gmail.com>
2021-10-13 15:07:32 +0100

    Listing 2.11

---
+++ 2.11.kt
@@ -0,0 +1,11 @@
+enum class Color (val r: Int, val g: Int, val b: Int) {
+
+    RED(255, 0, 0),
+    ORANGE(255, 165, 0),
+    YELLOW(255, 255, 0)
+    GREEN(0, 255, 0),
+    BLUE(0, 0, 255),
+    INDIGO(75, 0, 130),
+    VIOLET(238, 130, 238)
+
+}
